5cfeae7bb4db150a75d299285e451f3be13a3d21
Ameer Hamza Tariq <ahamzatariq@gmail.com>
2020-06-29 19:43:41 +0500

    New Module : Responsiveness & Adaptiveness The idea is to show both chart and list in portrait mode, but show a switch and either chart or list based on that switch mode. This is implemented using if() statements. New Concepts: - if() (This is a different kind of if statement and thus doesn't use curly braces. It's of a 'if inside of a list')

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text(
         'Personal Expenses App',
@@ -105,40 +107,51 @@ class _MyHomePageState extends State<MyHomePage> {
         )
       ],
     );
+    final txListWidget = Container(
+        height: (MediaQuery.of(context).size.height -
+                (appBar.preferredSize.height +
+                    MediaQuery.of(context).padding.top)) *
+            0.6,
+        child: TransactionList(_userTransactions, _deleteTransactions));
+
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('Show Chart'),
-                Switch(
-                  value: _showChart,
-                  onChanged: (value) {
-                    setState(() {
-                      _showChart = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            _showChart
-                ? Container(
-                    height: (MediaQuery.of(context).size.height -
-                            (appBar.preferredSize.height +
-                                MediaQuery.of(context).padding.top)) *
-                        0.7,
-                    child: Chart(_recentTransactions))
-                : Container(
-                    height: (MediaQuery.of(context).size.height -
-                            (appBar.preferredSize.height +
-                                MediaQuery.of(context).padding.top)) *
-                        0.6,
-                    child: TransactionList(
-                        _userTransactions, _deleteTransactions)),
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Show Chart'),
+                  Switch(
+                    value: _showChart,
+                    onChanged: (value) {
+                      setState(() {
+                        _showChart = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            if (!isLandscape)
+              Container(
+                  height: (MediaQuery.of(context).size.height -
+                          (appBar.preferredSize.height +
+                              MediaQuery.of(context).padding.top)) *
+                      0.3,
+                  child: Chart(_recentTransactions)),
+            if (!isLandscape) txListWidget,
+            if (isLandscape)
+              _showChart
+                  ? Container(
+                      height: (MediaQuery.of(context).size.height -
+                              (appBar.preferredSize.height +
+                                  MediaQuery.of(context).padding.top)) *
+                          0.7,
+                      child: Chart(_recentTransactions))
+                  : txListWidget
           ],
         ),
       ),
