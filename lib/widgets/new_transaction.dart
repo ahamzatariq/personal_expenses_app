16be5b28fe4e8f3fec7a28e1b91083b629fb9c60
Ameer Hamza Tariq <ahamzatariq@gmail.com>
2020-06-29 21:57:02 +0500

    New Module : Responsiveness & Adaptiveness The new widget acts as an example to code optimization, as how the code can be organized in more readable, and optimal form. This is the final version of the application of the module.

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:personalexpensesapp/widgets/adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -86,22 +85,7 @@ class _NewTransactionState extends State<NewTransaction> {
                           ? 'No Date Chosen!'
                           : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}'),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            onPressed: _presentDatePicker,
-                          )
-                        : FlatButton(
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            onPressed: _presentDatePicker,
-                            textColor: Theme.of(context).primaryColor,
-                          )
+                    AdaptiveFlatButton("Choose Date", _presentDatePicker)
                   ],
                 ),
               ),
